da6eb7410f7ec0459bde4b1da4e813b83575cbd9
TheyMightBeRobots <219560943+TheyMightBeRobots@users.noreply.github.com>
2025-07-06 11:42:02 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import rainbowhat as sh
+import rainbowhat as rh
 import asyncio
 import time
 
@@ -7,13 +7,13 @@ async def load():
     rh.rainbow.clear()
     rh.rainbow.set_pixel(pixel,255,255,255)
     rh.rainbow.show()
-    time.sleep(0.1)
+    await asyncio.sleep(0.1)
 
 async def song():
   song = [68,68,69,69,70,70,71,71]
   for note in song:
     rh.buzzer.midi_note(note,.5)
-    time.sleep(1)
+    await asyncio.sleep(1)
 
   rh.buzzer.midi_note(40,2)
 
